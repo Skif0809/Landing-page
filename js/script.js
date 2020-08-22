0cc4929428711eee9befe7c521239e0525b44454
Sergey <serg80997@mail.ru>
2020-08-22 13:40:28 +0300

    refactoring code 4

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Timer
 
-    let deadline = '2020-08-21';
+    let deadline = '2020-08-23';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -106,6 +106,26 @@ window.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = '';
     });
 });
+class Options {
+	constructor(height, width, bg, fontSize, textAlign) {
+		this.height = height;
+		this.width = width;
+		this.bg = bg;
+		this.fontSize = fontSize;
+		this.textAlign = textAlign;
+	}
+
+	createDiv() {
+		let elem = document.createElement('div');
+		document.body.appendChild(elem);
+		let param = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
+		elem.style.cssText = param;
+	}
+}
+
+const item = new Options(300, 350, "red", 14, "center");
+
+item.createDiv();
 
 // Второе задание
 
